6f0842736189ad96bc20cef27338a8e21f67a625
Smriti0405 <smritis0405@gmail.com>
2021-12-02 23:51:33 +0530

    Java

---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Calculator {
+    int num1;
+    int num2;
+    Calculator(){
+        Scanner s = new Scanner(System.in);
+        int num1 = s.nextInt();
+        int num2 = s.nextInt();
+    }
+}
