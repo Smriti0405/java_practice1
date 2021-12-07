0605991f05232d47ff48e46ed4af643a16430348
Smriti0405 <smritis0405@gmail.com>
2021-12-07 23:29:15 +0530

    Java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,16 @@
-import java.util.Scanner;
+public class Calculator{
+    public static void main(String[] args) {
 
-public class Calculator {
-    int num1;
-    int num2;
-    Calculator(){
-        Scanner s = new Scanner(System.in);
-        int num1 = s.nextInt();
-        int num2 = s.nextInt();
+        System.out.println(Calculate.powerInt(2, 3));
+        System.out.println(Calculate.powerDouble(3.0, 2.0));
+
+    }
+}
+class Calculate{
+    public static int powerInt(int num1, int num2){
+        return (int) Math.pow(num1, num2);
+    }
+    public static double powerDouble(double num1, double num2){
+        return Math.pow(num1, num2);
     }
 }
